a6b3cdf0b4c652ba550feef5cb18624723c012ad
Oskar Polak <oskar.polak@akademiakodu.pl>
2018-05-08 18:53:47 +0200

    Initial commit

---
+++ src/main/java/pl/oskarpolak/firstApp/controllers/MainController.java
@@ -0,0 +1,36 @@
+package pl.oskarpolak.firstApp.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "Oskar");
+        model.addAttribute("lastname", "Polak");
+        return "index";
+    }
+
+    @GetMapping("/can/i/{age}/{name}")
+    @ResponseBody
+    public String canI(@PathVariable("age") int age,
+                       @PathVariable("name") String name){
+        return age >= 18 ? name + ", możesz wejść do klubu" : name + ", no niestety";
+    }
+
+    @GetMapping("/club/{age}")
+    public String club(@PathVariable("age") int age,
+                       Model model){
+        model.addAttribute("infoText", age >= 18 ? "możesz wejść do klubu" :  "no niestety");
+        return "club";
+    }
+}
